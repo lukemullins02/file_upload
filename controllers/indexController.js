0a39d4cd4720ca26223cc9057a41bffb1d20e4b6
= <lukejmullins@gmail.com>
2026-03-27 10:54:12 -0400

    Finish css

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -17,6 +17,7 @@ const passwordLengthErr = " must be at least 8 characters long.";
 const mismatchErr =
   " must contain at least one uppercase letter, one lowercase letter, one number, and one symbol.";
 const passwordErr = "Passwords don't match.";
+const folderErr = "must be between 1 and 18 characters.";
 
 const validateNewUser = [
   body("username")
@@ -34,6 +35,10 @@ const validateNewUser = [
       return value === req.body.password;
     })
     .withMessage(`${passwordErr}`),
+];
+
+const validateFolder = [
+  body("name").isLength({ min: 1, max: 18 }).withMessage(`Folder ${folderErr}`),
 ];
 
 const getHome = async (req, res) => {
@@ -93,23 +98,23 @@ const getLogIn = (req, res) => {
   }
 };
 
-const postFileCloud = (req, res, next) => {
-  const files = db.getFiles(req.user.id, req.params.id);
+const postFileCloud = async (req, res, next) => {
+  const files = await db.getFiles(req.user.id, req.params.id);
 
   upload(req, res, function (err) {
     if (err instanceof multer.MulterError) {
       if (err.code === "LIMIT_FILE_SIZE") {
-        return res.status(400).render("folder.ejs", {
+        return res.status(400).render("folder", {
           errors: [{ msg: "File too large. Max 5MB." }],
           folder: { id: req.params.id },
-          files,
+          files: files,
         });
       }
     } else if (err) {
-      return res.status(400).render("folder.ejs", {
+      return res.status(400).render("folder", {
         errors: [{ msg: err.message }],
         folder: { id: req.params.id },
-        files,
+        files: files,
       });
     }
 
@@ -117,7 +122,6 @@ const postFileCloud = (req, res, next) => {
   });
 };
 const postFileForm = async (req, res) => {
-  console.log("hello", req.file);
   const { originalname, size, filename, path } = req.file;
 
   await db.createFile(
@@ -136,11 +140,22 @@ const getFolderForm = (req, res) => {
   res.render("forms/folder-form");
 };
 
-const postFolderForm = async (req, res) => {
-  const { name } = req.body;
-  await db.createFolder(name, req.user.id);
-  res.redirect("/");
-};
+const postFolderForm = [
+  validateFolder,
+  async (req, res) => {
+    const errors = validationResult(req).array();
+
+    if (errors.length != 0) {
+      return res.status(400).render("forms/folder-form", {
+        errors: errors,
+      });
+    }
+
+    const { name } = matchedData(req);
+    await db.createFolder(name, req.user.id);
+    res.redirect("/");
+  },
+];
 
 const getFolder = async (req, res) => {
   const folder = await db.getFolder(req.params.id);
@@ -175,6 +190,13 @@ const getDeleteFolder = async (req, res) => {
 
 const getFilePage = async (req, res) => {
   const file = await db.getFile(req.params.file_id);
+
+  file.uploadAt = new Intl.DateTimeFormat("en-US", {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+  }).format(file.uploadAt);
+
   res.render("file", {
     file: file,
   });
